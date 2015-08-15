f13e8d6ad7a54da4f82a3b2b30f684dae5143901
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-08-15 10:20:19 -0700

    Pass two specs.

--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -5,4 +5,8 @@ describe('String#rock_paper_scissors') do
   it('returns true when the first player wins') do
     expect('rock'.rock_paper_scissors('scissors')).to(eq(true))
   end
+  
+  it('returns 1 when there is a tie game') do
+    expect('scissors'.rock_paper_scissors('scissors')).to(eq(1))
+  end
 end
